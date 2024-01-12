1fb43bc8b7007a0604a8af4707fb18cb786c51cb
Amer Usman <148078810+amerusman@users.noreply.github.com>
2024-01-12 23:17:35 +0500

    Delete storage directory

--- storage/framework/views/1dd906c843afd7844fd5b005f070b848a384e02e.php
+++
@@ -1,3 +0,0 @@
-<?php echo $body; ?>
-
-<?php /**PATH /var/www/italyskincare.com/resources/views/components/block.blade.php ENDPATH**/ ?>
--- storage/framework/views/8224d615b5ede16c484177b491a59d67cff701ec.php
+++
@@ -1,3 +0,0 @@
-<span <?php echo e($attributes); ?>><?php echo Site::currency()->sign; ?></span> <?php echo e(Site::price($slot)); ?>
-
-<?php /**PATH /var/www/italyskincare.com/resources/views/components/price.blade.php ENDPATH**/ ?>
